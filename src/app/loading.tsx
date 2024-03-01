10e97ef6ab26427356c8c892e70b5516b597557d
jojo <perebaj@gmail.com>
2024-03-01 10:10:29 -0300

    :bug: chore: slate-100

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-slate-200">
+    <div className="flex min-h-screen items-center justify-center bg-slate-100">
       <h1 className="text-lg font-semibold">Carregando...</h1>
     </div>
   )

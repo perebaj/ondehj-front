ed77b9f4f21b315d880aabbd81e44bae9984b3b3
jojo <perebaj@gmail.com>
2024-04-02 15:53:31 -0300

    :bug: fix: hidden title lg screen

--- src/app/landing/header.tsx
+++ src/app/landing/header.tsx
@@ -8,8 +8,8 @@ export default function Header() {
     <header>
       <nav className="mx-auto flex max-w-7xl items-center justify-center px-8 py-4">
         <div className="flex lg:flex-1">
-          <Link href="/" className="flex shrink-0 items-center">
-            <span className="text-2xl font-extrabold text-slate-900">
+          <Link href="/" className="hidden shrink-0 items-center lg:flex">
+            <span className="text-2xl font-extrabold text-slate-900 ">
               Onde Hoje?
             </span>
           </Link>

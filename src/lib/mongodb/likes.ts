3403e21c3ab8924e2b3534872ad3647bb36f8723
jojo <perebaj@gmail.com>
2024-04-02 15:45:57 -0300

    :bug: fix: build

--- src/lib/mongodb/likes.ts
+++
@@ -1,3 +0,0 @@
-// This file will contain the logic for the likes collection in MongoDB. A user can like an event,
-// and this should be reflected in the database. The likes collection will contain the event ID and
-'use server'

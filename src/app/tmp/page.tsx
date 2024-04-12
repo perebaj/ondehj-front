d88bde579a6aeedd21c9f5dbb4eaf5eda80fa5a0
jojo <perebaj@gmail.com>
2024-04-12 13:37:34 -0300

    :sparkles: fix build error

--- src/app/tmp/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getUsers } from '@/lib/postgres/user'
-
-export default async function Home() {
-  const resp = await getUsers()
-  console.log(resp)
-  return <div>JOjo is awesome</div>
-}
